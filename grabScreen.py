8f370957fba67a2b1eda54b7a374215bd9cd30f8
jano.skvara <jano.skvara@outlook.sk>
2021-12-06 17:09:06 +0100

    first commit

---
+++ grabScreen.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import win32gui, win32ui, win32con, win32api
+
+
+def grab_screen(region=None):
+
+    hwin = win32gui.GetDesktopWindow()
+
+    if region:
+            left, top, width ,height = region
+    else:
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height, width, 4)
+
+    #cv2.imwrite("imageFULL.png", img) 
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
---
+++ openingChrome.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import cv2
+import os
+import keyboard
+from grabScreen import grab_screen
+
+_x_position = 0
+_y_position = 0
+width = 800
+height = 600
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.set_window_rect(_x_position, _y_position, width , height)
+
+# get chromedinoS
+driver.get("https://chromedino.com")
+ 
+# set window position
+count = 0
+pic = 1
+
+# Point to where we want the images to be saved
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "img\\")
+
+_up_path = r'.\dataset\dup' 
+if not os.path.exists(_up_path):
+    os.makedirs(_up_path)
+
+_down_path = r'.\dataset\ddown' 
+if not os.path.exists(_down_path):
+    os.makedirs(_down_path)
+
+_nothing_path = r'.\dataset\dnothing' 
+if not os.path.exists(_nothing_path):
+    os.makedirs(_nothing_path)
+
+# Collets images until you press e
+while not keyboard.is_pressed("e"):
+    count += 1
+    image = grab_screen(region = (_x_position + 100, _y_position+300, width, 200))
+    #Covert to grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Cover to black or white pixel
+    (thresh, image) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow('Bot View', image)
+    cv2.waitKey(1)
+
+    if keyboard.is_pressed(' '):
+        cv2.imwrite(f"{_up_path}\BotView{count}.jpg", image)
+        continue
+
+    if keyboard.is_pressed('down'):
+        cv2.imwrite(f"{_down_path}\BotView{count}.jpg", image)
+        continue
+
+    if count%20 == 0:
+        cv2.imwrite(f"{_nothing_path}\BotView{count}.jpg", image)
