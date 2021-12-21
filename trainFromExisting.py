ee37a34144b255db4a1f542f14d6fd72f3f35d88
vjavorek <javorek.v@gmail.com>
2021-12-21 17:45:44 +0100

    modelpath update for testing

--- trainFromExisting.py
+++ trainFromExisting.py
@@ -1,15 +1,18 @@
 from fastai.vision.all import *
+from pathlib import Path
 
 def label_func(x): return x.parent.name
+
+arch = resnet18
 
 def train():
     path = r'.\dataset'
     namesList = get_image_files(path)
     print(f"Total Images:{len(namesList)}")
 
-    dls = ImageDataLoaders.from_path_func(path, namesList, label_func, bs=40)
+    dls = ImageDataLoaders.from_path_func(path, namesList, label_func, bs=16)
 
-    learn = cnn_learner(dls, resnet18, metrics=accuracy)
+    learn = cnn_learner(dls, arch, pretrained=True, metrics=accuracy)
     print("Loaded")
     return(learn)
 
@@ -20,5 +23,7 @@ if __name__ == '__main__':
     #learner.lr_find()
     #learner.fine_tune(2, 3e-3)
     #learner.show_results()
-    learner.fit(5)
-    learner.export('model.pkl')
+    learner.fit(1)
+    modelpath = Path(__file__).parent.resolve() / f"jobData/model_{str(arch)}.pkl"
+    print(modelpath)
+    learner.export(modelpath)
