527bd95274c76ad6a753a9e5af0504a0b6fc735b
oValach <valach.ondra@seznam.cz>
2021-12-25 14:49:17 +0100

    data cleared

--- trainFromExisting.py
+++ trainFromExisting.py
@@ -5,11 +5,11 @@ from PIL import Image
 def label_func(x): return x.parent.name
 
 arch = resnet18
-epochs = 6
+epochs = 1
 archname = str(arch)
 name = archname[10:int(len(archname))-23]
 
-def train():uhik
+def train():
     path = r'.\dataset'
     namesList = get_image_files(path)
     print(f"Total Images:{len(namesList)}")
@@ -29,13 +29,22 @@ def train():uhik
     return(learn)
 
 if __name__ == '__main__':
+
     learner = train()
-    #interp = ClassificationInterpretation.from_learner(learner)
-    #interp.plot_confusion_matrix(figsize=(6,6))
+    learner.fit(epochs)
+
+    modelpath = Path(__file__).parent.resolve() / f"jobData/model_{name}-{str(epochs)}.pkl"
+    learner.export(modelpath)
+
+    learner.show_results()
+    interp = ClassificationInterpretation.from_learner(learner)
+    interp.plot_confusion_matrix(figsize=(6,6))
+    interp.plot_top_losses(20, figsize=(10,10))
+    try:
+        interp.print_classification_report()
+    except:
+        pass
+
     #learner.lr_find()
     #learner.fine_tune(2, 3e-3)
-    #learner.show_results()
-    learner.fit(epochs)
-    modelpath = Path(__file__).parent.resolve() / f"jobData/model_{name}-{str(epochs)}.pkl"
-    print(modelpath)
-    learner.export(modelpath)
+    
