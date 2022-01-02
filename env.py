83ab7d581601625a8745cccc9e8263899d91063e
janskvara <jano.skvara@outlook.sk>
2022-01-02 17:46:21 +0100

    new dqn

--- env.py
+++ env.py
@@ -1,3 +1,86 @@
+import numpy as np
+#import gym
+import collections
+import cv2
+
+
+'''class RepeatActionAndMaxFrame(gym.Wrapper):
+
+    def __init__(self, env=None, repeat=4):
+        super(RepeatActionAndMaxFrame, self).__init__(env)
+        self.repeat = repeat
+        self.shape = env.observation_space.low.shape
+        self.frame_buffer = np.zeros_like((2,self.shape), dtype=object)
+
+    def step(self, action):
+        t_reward = 0.0
+        done = False
+        for i in range(self.repeat):
+            obs, reward, done, info = self.env.step(action)
+            t_reward += reward
+            idx = i % 2
+            self.frame_buffer[idx] = obs
+            if done:
+                break
+
+        max_frame = np.maximum(self.frame_buffer[0], self.frame_buffer[1])
+        return max_frame, t_reward, done, info
+
+    def reset(self):
+        obs = self.env.reset()
+        self.frame_buffer = np.zeros_like((2,self.shape), dtype=object)
+        self.frame_buffer[0] = obs
+        return obs
+
+class PreprocessFrame(gym.ObservationWrapper):
+    def __init__(self, shape, env=None):
+        super(PreprocessFrame, self).__init__(env)
+        self.shape=(shape[2], shape[0], shape[1])
+        self.observation_space = gym.spaces.Box(low=np.float32(0), high=np.float32(1.0),
+                                              shape=self.shape, dtype=np.float32)
+    def observation(self, obs):
+        new_frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+        resized_screen = cv2.resize(new_frame, self.shape[1:],
+                                    interpolation=cv2.INTER_AREA)
+
+        new_obs = np.array(resized_screen, dtype=np.uint8).reshape(self.shape)
+        new_obs = np.swapaxes(new_obs, 2,0)
+        new_obs = new_obs / 255.0
+        return new_obs
+
+class StackFrames(gym.ObservationWrapper):
+    def __init__(self, env, n_steps):
+        super(StackFrames, self).__init__(env)
+        self.observation_space = gym.spaces.Box(
+                             np.float32(env.observation_space.low.repeat(n_steps, axis=0)),
+                             np.float32(env.observation_space.high.repeat(n_steps, axis=0)),
+                             dtype=np.float32)
+        self.stack = collections.deque(maxlen=n_steps)
+
+    def reset(self):
+        self.stack.clear()
+        observation = self.env.reset()
+        for _ in range(self.stack.maxlen):
+            self.stack.append(observation)
+
+        return np.array(self.stack).reshape(self.observation_space.low.shape)
+
+    def observation(self, observation):
+        self.stack.append(observation)
+        obs = np.array(self.stack).reshape(self.observation_space.low.shape)
+
+        return obs
+
+def make_env(env_name, shape=(84,84,1), skip=4):
+    np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
+    env = gym.make(env_name)
+    env = RepeatActionAndMaxFrame(env, skip)
+    env = PreprocessFrame(shape, env)
+    env = StackFrames(env, skip)
+
+    return env'''
+
+
 import cv2
 import numpy as np
 import win32gui, win32ui, win32con
@@ -10,6 +93,7 @@ import time
 class Environment():
 
     pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'  # your path may be different
+    # pytesseract.pytesseract.tesseract_cmd = r'D:/Program Files (x86)/TesseractOCR/tesseract.exe'
     _window_x_position = 0
     _window_y_position = 0
     _window_width = 800
@@ -18,10 +102,10 @@ class Environment():
     _x_position = 0
     _y_position = 0
 
+
     width = 0 
     height = 0
     action_space = np.array([0.0, 1.0, 2.0], dtype=np.float32) #["up", "down", "none"]
-
     actual_reward = 0
 
     def __init__(self):
@@ -33,11 +117,16 @@ class Environment():
         self._y_position = self._window_y_position + 220
         self.width =  self._window_width - 100
         self.height =  150
+        #shape=(84,84,1)
+        #self.shape=(shape[2], shape[0], shape[1])
+        self.shape = (1, self.height, self.width)
         self._game_over_image = cv2.imread(r'jobData\restart.png')
     
-    def reset(self):
+    def reset(self) -> any:
         time.sleep(0.3)
         keyboard.press_and_release('space') #game start
+        self.actual_image = self.getScreen()
+        return self.getWhiteBlackScreen()
 
     def close(self):
         print("ZATVARAME :)")
@@ -55,10 +144,11 @@ class Environment():
             #do nothing
 
         self.actual_image = self.getScreen()
+        self.black_white_image = self.getWhiteBlackScreen()
         self.actual_reward = self.getReward()
         self.done = self.getDone()
 
-        return self.actual_image, self.actual_reward, self.done
+        return self.black_white_image, self.actual_reward, self.done
 
     def getDone(self) -> bool:
 
