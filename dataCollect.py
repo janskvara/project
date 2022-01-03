9a33a09dc696077ae691f05b1c8022ae2df31db8
janskvara <jano.skvara@outlook.sk>
2022-01-03 23:17:00 +0100

    for grabing data (84 pixels)

--- dataCollect.py
+++ dataCollect.py
@@ -9,15 +9,15 @@ import uuid
 # set window position
 count = 0
 
-_up_path = r'.\dataset\dup' 
+_up_path = r'.\dataset\dup84' 
 if not os.path.exists(_up_path):
     os.makedirs(_up_path)
 
-_down_path = r'.\dataset\ddown' 
+_down_path = r'.\dataset\ddown84' 
 if not os.path.exists(_down_path):
     os.makedirs(_down_path)
 
-_nothing_path = r'.\dataset\dnothing' 
+_nothing_path = r'.\dataset\dnothing84' 
 if not os.path.exists(_nothing_path):
     os.makedirs(_nothing_path)
 
@@ -26,18 +26,19 @@ environment = Environment()
 # Collets images until you press e
 while not keyboard.is_pressed("e"):
     count += 1
+    environment.getScreen()
     image = environment.getWhiteBlackScreen()
 
-    cv2.imshow('Bot View', image)
+    new_frame = cv2.resize(image, (840,840), interpolation=cv2.INTER_AREA)
+    cv2.imshow('Bot View', new_frame)
     cv2.waitKey(1)
 
     if keyboard.is_pressed(' ') or keyboard.is_pressed('up'):
         cv2.imwrite(f"{_up_path}\{uuid.uuid4()}.jpg", image)
         continue
 
-    elif keyboard.is_pressed('down'):
+    if keyboard.is_pressed('down'):
         cv2.imwrite(f"{_down_path}\{uuid.uuid4()}.jpg", image)
         continue
-    
-    else:
-        cv2.imwrite(f"{_nothing_path}\{uuid.uuid4()}.jpg", image)
+
+    cv2.imwrite(f"{_nothing_path}\{uuid.uuid4()}.jpg", image)
--- trainedAgent.py
+++ trainedAgent.py
@@ -6,68 +6,43 @@ import keyboard
 import time
 import pyautogui as pyautogui
 import cv2
+import pathlib
 
 def label_func(x): return x.parent.name
 
-def scan_for_restart(): # scans for the game restart
-    result = pyautogui.locateOnScreen(r'jobData\restart.png', grayscale=True, region=(100, 150, 400, 350), confidence=0.9) 
-    if result != None: # restart button located
-        time.sleep(1)
-        print('RESTART')
-        keyboard.press_and_release('space')
 
 def load(path): # loads trained model
     learner = load_learner(path)
     print('Model loaded.')
     return(learner)
-
-def decide(learner): 
-    image = environment.getWhiteBlackScreen()
-    cv2.imshow('Bot View', image) 
-    cv2.waitKey(1)
-
-    # Decide about next step
-    result = learner.predict(image)
-    action = result[0]
-
-    return action #ddown / dnothing / dup
-
-def run():
-    prevKey = None
-    keyboard.press_and_release('space') #game start
     
-    # decide about next moves until user presses 'e' key
+def run(learner):
+    
+    env = Environment()
+
+    image = env.reset()
+
     while not keyboard.is_pressed("e"):
+        
+        cv2.imshow('Bot View', image)
+        cv2.waitKey(1)
 
-        #scan_for_restart()
-        action = decide(learner)
+        result = learner.predict(image)
+        action = result[0]
 
-        # transfer from directory name to key name
-        if action == 'ddown':
-            keyToPress = 'down'
-        elif action == 'dup':
-            keyToPress = 'up'
+        if action == 'dup84':
+            keyToPress = 0.0
+        elif action == 'ddown84':
+            keyToPress = 1.0
         else:
-            continue # do nothing
+            keyToPress = 2.0
 
-        # key hold or switch of the key that is being held
-        keyboard.press_and_release(keyToPress)
-        '''if keyToPress == prevKey:
-            continue
-        elif keyToPress != prevKey:
-            if prevKey != None:
-                keyboard.release(prevKey)
-            keyboard.press(keyToPress)
-            print('Input: ' + keyToPress)
-        prevKey = keyToPress'''
+        image, reward, done =  env.step(keyToPress)
 
 if __name__ == '__main__':
-    
-    # for models trained on colab
-    import pathlib
     temp = pathlib.PosixPath
     pathlib.PosixPath = pathlib.WindowsPath
-    
-    environment = Environment()
-    learner = load('jobData\model_resn-6.pkl')
-    run()
+    path = os.path.join('C:\SDU\AIG\project\jobData', 'model_squeezene-3_84_150k.pkl')
+    learner = load(path)
+    run(learner)
+
