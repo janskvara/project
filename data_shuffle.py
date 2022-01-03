12ce942819f7d52bc3337eff07fc9c30b1537298
janskvara <jano.skvara@outlook.sk>
2022-01-03 21:34:19 +0100

    cleaning

--- data_shuffle.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-import shutil
-
-src_dir = 'dataset\ddown'
-dst_dir = 'dataset\data_shuffled'
-for png in glob.iglob(os.path.join(src_dir, "*.png")):
-    shutil.copy(png, dst_dir)
